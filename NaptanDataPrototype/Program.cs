188411c99b2f482ece8656a5108392023d9098fd
Kam <kam.sangarapandian@tesco.com>
2023-02-01 14:36:40 +0000

    updated program to run for all stop points and finding how many mismatches

--- NaptanDataPrototype/Program.cs
+++ NaptanDataPrototype/Program.cs
@@ -1,23 +1,52 @@
-﻿using NaptanDataPrototype.Functions;
+﻿using System.Diagnostics;
+using NaptanDataPrototype.Functions;
 using NaptanDataPrototype.Services;
 
 var naptanData = new NaptonXmlFileService();
 
-var xmlLocation = naptanData.GetLocation(@"./Files/Naptan-oneStopPoint.xml");
+var xmlLocations = naptanData.GetLocation(@"./Files/Brighton.xml");
 
 var bng2latlongService = new OsToLatLonService();
 
-var locationService = await bng2latlongService.GetLatitudeLongitude(xmlLocation[0].Easting, xmlLocation[0].Northing);
+var misMatchCount = 0;
+var misMatchLatitudeCount = 0;
+var misMatchLongitudeCount = 0;
 
-if(Functions.IsMatching(locationService.Latitude, xmlLocation[0].TruncatedLatitude)
-   && Functions.IsMatching(locationService.Longitude, xmlLocation[0].TruncatedLongitude, 0.00000)
-   )
+var stopWatch = new Stopwatch();
+stopWatch.Start();
+foreach (var xmlLocation in xmlLocations)
 {
-    Console.WriteLine("Matching lat/long");
+    var locationService = await bng2latlongService.GetLatitudeLongitude(xmlLocation.Easting, xmlLocation.Northing);
+
+    if (!Functions.IsMatching(locationService.Latitude, xmlLocation.TruncatedLatitude, 0.00005)
+        || !Functions.IsMatching(locationService.Longitude, xmlLocation.TruncatedLongitude, 0.00005))
+    {
+        misMatchCount++;
+        
+        if(!Functions.IsMatching(locationService.Latitude, xmlLocation.TruncatedLatitude))
+        {
+            misMatchLatitudeCount++;
+            Console.WriteLine("MisMatching latitude");
+        }
+    
+        if(!Functions.IsMatching(locationService.Longitude, xmlLocation.TruncatedLongitude, 0.00000))
+        {
+            misMatchLongitudeCount++;
+            Console.WriteLine("MisMatching longitude");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Matching lat/long");
+    }
+    
+    // Console.WriteLine($"XML Latitude value = {xmlLocation.TruncatedLatitude}, Converted Latitude = {locationService.Latitude}");
+    // Console.WriteLine($"XML Longitude value = {xmlLocation.TruncatedLongitude}, Converted Longitude = {locationService.Longitude}");
 }
-else
-{
-    Console.WriteLine("Mismatching lat/long");
-}
-// Console.WriteLine($"XML Latitude value = {xmlLocation.TruncatedLatitude}, Converted Latitude = {locationService.Latitude}");
-// Console.WriteLine($"XML Longitude value = {xmlLocation.TruncatedLongitude}, Converted Longitude = {locationService.Longitude}");
+stopWatch.Stop();
+
+Console.WriteLine($"Total count = {xmlLocations.Count}");
+Console.WriteLine($"Mismatch count = {misMatchCount}");
+Console.WriteLine($"Mismatch Latitude count = {misMatchLatitudeCount}");
+Console.WriteLine($"Mismatch Longitude count = {misMatchLongitudeCount}");
+Console.WriteLine($"Total time took to run the file = {stopWatch.Elapsed.Hours}h:{stopWatch.Elapsed.Minutes}m:{stopWatch.Elapsed.Seconds}s");
