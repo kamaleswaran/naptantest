ad486a1f76f4c4677240dea30019b0f929eda917
Kam <kam.sangarapandian@tesco.com>
2023-02-03 19:12:01 +0000

    refactoring

--- NaptanDataPrototype/Program.cs
+++ NaptanDataPrototype/Program.cs
@@ -27,16 +27,11 @@ stopWatch.Start();
 double acceptableDifference = 0.00002;
 int totalProcessed = 0;
 
-//object lockObject = new object();
-
+//foreach (var xmlLocation in xmlLocations) //takes 1h 43m to complete
 await Parallel.ForEachAsync(xmlLocations, async (xmlLocation, token) =>
 {
-    //lock (lockObject)
-    //{
-        totalProcessed++;
-        Console.WriteLine(totalProcessed);
-    //}
-    //Log.Information($"Running.... {stopWatch.Elapsed.Hours}h:{stopWatch.Elapsed.Minutes}m:{stopWatch.Elapsed.Seconds}s:{stopWatch.ElapsedMilliseconds}ms");
+    totalProcessed++;
+    Console.WriteLine(totalProcessed);
     var locationService = await bng2latlongService.GetLatitudeLongitude(xmlLocation.Easting, xmlLocation.Northing);
 
     if (locationService == null)
@@ -46,17 +41,19 @@ await Parallel.ForEachAsync(xmlLocations, async (xmlLocation, token) =>
         || !Functions.IsMatchingValues(locationService.Longitude, xmlLocation.Longitude, acceptableDifference))
     {
         misMatchCountDictionary = Functions.MismatchCountIncrement(misMatchCountDictionary, xmlLocation.AtcoCode);
-        
-        if(!Functions.IsMatchingValues(xmlLocation.Latitude, locationService.Latitude, acceptableDifference))
+
+        if (!Functions.IsMatchingValues(xmlLocation.Latitude, locationService.Latitude, acceptableDifference))
         {
             misMatchLatitudeCount = Functions.MismatchCountIncrement(misMatchLatitudeCount, xmlLocation.AtcoCode);
-            Log.Information($"MisMatching latitude. XML AtcoCode = {xmlLocation.AtcoCode}, XML Latitude value = {xmlLocation.Latitude}, Converted Latitude = {locationService.Latitude}");
+            Log.Information(
+                $"MisMatching latitude. XML AtcoCode = {xmlLocation.AtcoCode}, XML Latitude value = {xmlLocation.Latitude}, Converted Latitude = {locationService.Latitude}");
         }
-    
-        if(!Functions.IsMatchingValues(xmlLocation.Longitude, locationService.Longitude, acceptableDifference))
+
+        if (!Functions.IsMatchingValues(xmlLocation.Longitude, locationService.Longitude, acceptableDifference))
         {
             misMatchLongitudeCount = Functions.MismatchCountIncrement(misMatchLongitudeCount, xmlLocation.AtcoCode);
-            Log.Information($"MisMatching longitude. XML AtcoCode = {xmlLocation.AtcoCode}, XML Longitude value = {xmlLocation.Longitude}, Converted Longitude = {locationService.Longitude}");
+            Log.Information(
+                $"MisMatching longitude. XML AtcoCode = {xmlLocation.AtcoCode}, XML Longitude value = {xmlLocation.Longitude}, Converted Longitude = {locationService.Longitude}");
         }
     }
 });
@@ -68,17 +65,17 @@ Log.Information($"Total processed count = {totalProcessed}");
 
 foreach (var key in misMatchCountDictionary.Keys)
 {
-    Log.Information($"Key: {key}. MismatchCount = {misMatchCountDictionary[key]}");
+    Log.Information($"AtcoCode: {key}. MismatchCount: {misMatchCountDictionary[key]}");
 }
 
 foreach (var key in misMatchLatitudeCount.Keys)
 {
-    Log.Information($"Key: {key}. MismatchLatitudeCount = {misMatchLatitudeCount[key]}");
+    Log.Information($"AtcoCode: {key}. MismatchLatitudeCount: {misMatchLatitudeCount[key]}");
 }
 
 foreach (var key in misMatchLongitudeCount.Keys)
 {
-    Log.Information($"Key: {key}. MismatchLongitudeCount = {misMatchLongitudeCount[key]}");
+    Log.Information($"AtcoCode: {key}. MismatchLongitudeCount: {misMatchLongitudeCount[key]}");
 }
 
 Log.Information($"Total time took to run the file = {stopWatch.Elapsed.Hours}h:{stopWatch.Elapsed.Minutes}m:{stopWatch.Elapsed.Seconds}s");
